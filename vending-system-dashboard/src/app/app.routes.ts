80372dcc3e97861456e2fae657bef7fc6f412a3d
8itamin <61024289+8itamin@users.noreply.github.com>
2026-02-06 18:02:45 +0300

    Fix Angular build errors in automats dashboard

--- vending-system-dashboard/src/app/app.routes.ts
+++ vending-system-dashboard/src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { AutomatsComponent } from './automats/automats';
+
+export const routes: Routes = [{ path: '', component: AutomatsComponent }];
